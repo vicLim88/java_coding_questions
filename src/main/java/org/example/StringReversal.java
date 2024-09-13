8e186bace10d441769a6eb9e2152da97e58ba807
vicLim88 <120290960+vicLim88@users.noreply.github.com>
2024-09-13 16:11:12 +0800

    First Commit

---
+++ src/main/java/org/example/StringReversal.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class StringReversal {
+    private final String strToReverse;
+
+    public StringReversal(String strToReverse){
+        this.strToReverse = strToReverse;
+    }
+
+    public String getReversedStr(){
+        StringBuilder sb = new StringBuilder();
+        for(int i = this.strToReverse.length()-1; i>=0; i--){
+            sb.append(this.strToReverse.charAt(i));
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/vic/techquestions/TestCase001.java
@@ -0,0 +1,21 @@
+package com.vic.techquestions;
+
+import org.junit.jupiter.api.Test;
+
+import org.example.StringReversal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCase001 {
+    private final String test_data = "Bubble";
+    private final String expected = "elbbuB";
+
+    @Test
+    public void test
+
+    @Test
+    public void testReverseString(){
+        StringReversal sR = new StringReversal(this.test_data);
+        assertEquals(this.expected, sR.getReversedStr());
+    }
+}
